816e23a49d5bde673dc845f4f5192492e7e7494d
Sergey Lyubimov <lysevi@gmail.com>
2017-06-02 07:32:28 +0300

    reconnection: test for many clients.

--- unittests/async_connection_test.cpp
+++ unittests/async_connection_test.cpp
@@ -16,136 +16,164 @@ using namespace boost::asio;
 
 using namespace dqueue;
 using namespace dqueue::utils;
+namespace {
+	struct testable_client : public AbstractClient {
+		size_t message_one = 0;
 
-struct testable_client : public AbstractClient {
-  size_t message_one = 0;
+		testable_client(boost::asio::io_service *service, const AbstractClient::Params &p)
+			: AbstractClient(service, p) {}
 
-  testable_client(boost::asio::io_service *service, const AbstractClient::Params &p)
-      : AbstractClient(service, p) {}
+		~testable_client() {}
 
-  ~testable_client() {}
+		void onConnect() {
+			logger_info("client: send hello ");
 
-  void onConnect() {
-    logger_info("client: send hello ");
+			auto nd = std::make_shared<NetworkMessage>(1);
 
-    auto nd = std::make_shared<NetworkMessage>(1);
+			this->_async_connection->send(nd);
+		}
 
-    this->_async_connection->send(nd);
-  }
+		void onNewMessage(const NetworkMessage_ptr &d, bool & /*cancel*/) override {
+			auto qh = reinterpret_cast<message_header *>(d->data);
 
-  void onNewMessage(const NetworkMessage_ptr &d, bool & /*cancel*/) override {
-    auto qh = reinterpret_cast<message_header *>(d->data);
+			int kind = (NetworkMessage::message_kind)qh->kind;
+			switch (kind) {
+			case 1: {
+				message_one++;
+				this->_async_connection->send(d);
+				break;
+			}
+			default:
+				dqueue::logger_fatal("server: unknow query kind - ", (int)kind);
+				break;
+			}
+		}
+	};
 
-    int kind = (NetworkMessage::message_kind)qh->kind;
-    switch (kind) {
-    case 1: {
-      message_one++;
-      this->_async_connection->send(d);
-      break;
-    }
-    default:
-      dqueue::logger_fatal("server: unknow query kind - ", (int)kind);
-      break;
-    }
-  }
-};
+	struct testable_server : public AbstractServer {
+		size_t message_one = 0;
 
-struct testable_server : public AbstractServer {
-  size_t message_one = 0;
+		testable_server(boost::asio::io_service *service, const AbstractServer::params &p)
+			: AbstractServer(service, p) {}
 
-  testable_server(boost::asio::io_service *service, const AbstractServer::params &p)
-      : AbstractServer(service, p) {}
+		~testable_server() {}
+		/* void onNetworkError(const boost::system::error_code &err) {
+		   THROW_EXCEPTION("error on - ", err.message());
+		 }*/
 
-  ~testable_server() {}
-  /* void onNetworkError(const boost::system::error_code &err) {
-     THROW_EXCEPTION("error on - ", err.message());
-   }*/
+		void onNewMessage(AbstractServer::io &io, const NetworkMessage_ptr &d, bool &cancel) {
+			auto qh = reinterpret_cast<message_header *>(d->data);
 
-  void onNewMessage(AbstractServer::io &io, const NetworkMessage_ptr &d, bool &cancel) {
-    auto qh = reinterpret_cast<message_header *>(d->data);
+			int kind = (NetworkMessage::message_kind)qh->kind;
+			switch (kind) {
+			case 1: {
+				message_one++;
+				io._async_connection->send(d);
+				break;
+			}
+			default:
+				dqueue::logger_fatal("server: unknow query kind - ", (int)kind);
+				break;
+			}
+		}
 
-    int kind = (NetworkMessage::message_kind)qh->kind;
-    switch (kind) {
-    case 1: {
-      message_one++;
-      io._async_connection->send(d);
-      break;
-    }
-    default:
-      dqueue::logger_fatal("server: unknow query kind - ", (int)kind);
-      break;
-    }
-  }
+		void onNetworkError(const boost::system::error_code &err) override {}
+	};
 
-  void onNetworkError(const boost::system::error_code &err) override {}
-};
+	bool server_stop = false;
+	std::shared_ptr<testable_server> server = nullptr;
+	void server_thread() {
+		boost::asio::io_service service;
+		AbstractServer::params p;
+		p.port = 4040;
+		server = std::make_shared<testable_server>(&service, p);
 
-bool server_stop = false;
-std::shared_ptr<testable_server> server = nullptr;
-void server_thread() {
-  boost::asio::io_service service;
-  AbstractServer::params p;
-  p.port = 4040;
-  server = std::make_shared<testable_server>(&service, p);
+		server->serverStart();
+		while (!server_stop) {
+			service.poll_one();
+		}
+		server = nullptr;
+	}
 
-  server->serverStart();
-  while (!server_stop) {
-    service.poll_one();
-  }
-  server = nullptr;
+	void testForReconnection(const size_t clients_count) {
+		boost::asio::io_service service;
+		AbstractClient::Params p;
+		p.host = "localhost";
+		p.port = 4040;
+		std::vector<std::shared_ptr<testable_client>> clients(clients_count);
+		for (size_t i = 0; i < clients_count; i++) {
+			clients[i] = std::make_shared<testable_client>(&service, p);
+			clients[i]->async_connect();
+		}
+		server_stop = false;
+		std::thread t(server_thread);
+		while (server == nullptr || !server->is_started) {
+			logger_info("!server->is_started serverIsNull? ", server == nullptr);
+			service.poll_one();
+		}
+
+		for (auto&c : clients) {
+			while (!c->isConnected) {
+				logger_info("client not connected");
+				service.poll_one();
+			}
+		}
+		for (auto&c : clients) {
+			while (server->message_one < 10 && c->message_one < 10) {
+				logger_info("server.message_one: ", server->message_one,
+					" client.message_one: ", c->message_one);
+				service.poll_one();
+			}
+		}
+		server_stop = true;
+		while (server != nullptr) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
+		t.join();
+
+		for (auto&c : clients) {
+			while (c->isConnected) {
+				logger_info("client is still connected");
+				service.poll_one();
+			}
+		}
+
+		// wait auto recconection on client.
+		server_stop = false;
+		t = std::thread(server_thread);
+		for (auto&c : clients) {
+			while (!c->isConnected && (server == nullptr || !server->is_started)) {
+				logger_info("client and server is not connected");
+				service.poll_one();
+			}
+		}
+
+		// disconnect from server
+		for (auto&c : clients) {
+			c->disconnect();
+		}
+		for (auto&c : clients) {
+			while (c->isConnected) {
+				logger_info("client is still connected");
+				service.poll_one();
+			}
+		}
+		// stop server
+		server_stop = true;
+		while (server != nullptr) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
+		t.join();
+	}
 }
 
-TEST_CASE("reconnetion") {
-  boost::asio::io_service service;
-  AbstractClient::Params p;
-  p.host = "localhost";
-  p.port = 4040;
+TEST_CASE("reconnetion.1") {
+	const size_t connections_count = 1;
+	testForReconnection(connections_count);
+}
 
-  auto client=std::make_shared<testable_client>(&service, p);
-  std::thread t(server_thread);
-  client->async_connect();
 
-  while (!client->isConnected && (server == nullptr || !server->is_started)) {
-    logger_info("client and server is not connected");
-    service.poll_one();
-  }
-
-  while (server->message_one < 10 && client->message_one < 10) {
-    logger_info("server.message_one: ", server->message_one,
-                " client.message_one: ", client->message_one);
-    service.poll_one();
-  }
-  server_stop = true;
-  while (server != nullptr) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  }
-  t.join();
-
-  while (client->isConnected) {
-    logger_info("client is still connected");
-    service.poll_one();
-  }
-
-  // wait auto recconection on client.
-  server_stop = false;
-  t = std::thread(server_thread);
-
-  while (!client->isConnected && (server == nullptr || !server->is_started)) {
-    logger_info("client and server is not connected");
-    service.poll_one();
-  }
-
-  // disconnect from server
-
-  client->disconnect();
-  while (client->isConnected) {
-    logger_info("client is still connected");
-    service.poll_one();
-  }
-  // stop server
-  server_stop = true;
-  while (server != nullptr) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  }
-  t.join();
+TEST_CASE("reconnetion.10") {
+	const size_t connections_count = 10;
+	testForReconnection(connections_count);
 }

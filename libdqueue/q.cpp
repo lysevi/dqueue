ca4ae396f34bfaaf876443e961c392d931a8df6a
Sergey Lyubimov <lysevi@gmail.com>
2017-06-21 08:17:47 +0300

    QueueSettings: use scheme.

--- libdqueue/q.cpp
+++ libdqueue/q.cpp
@@ -1,6 +1,7 @@
 #include <libdqueue/q.h>
-#include <cstring>
+#include <libdqueue/serialisation.h>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <cstring>
 
 const boost::posix_time::ptime START = boost::posix_time::from_time_t(0);
 using namespace dqueue;
@@ -16,26 +17,20 @@ uint64_t current_time() {
   auto now = boost::posix_time::microsec_clock::universal_time();
   return from_ptime(now);
 }
+using QueueSettingsScheme = serialisation::Scheme<std::string>;
 } // namespace
 
-NetworkMessage_ptr QueueSettings::toNetworkMessage()const {
-	uint32_t len = static_cast<uint32_t>(name.size());
-	uint32_t neededSize = sizeof(uint32_t) + len;
-	auto nd = std::make_shared<NetworkMessage>(neededSize, 0);
-	memcpy(nd->value(), &(len), sizeof(uint32_t));
-	memcpy(nd->value()+ sizeof(uint32_t), this->name.data(), this->name.size());
-	return nd;
+NetworkMessage_ptr QueueSettings::toNetworkMessage() const {
+  auto neededSize = QueueSettingsScheme::capacity(name);
+  auto nd = std::make_shared<NetworkMessage>(neededSize, 0);
+  QueueSettingsScheme::write(nd->value(), name);
+  return nd;
 }
 
-QueueSettings QueueSettings::fromNetworkMessage(const NetworkMessage_ptr&nd) {
-	uint32_t len = 0;
-	memcpy(&len, nd->value(), sizeof(uint32_t));
-	ENSURE(len > uint32_t());
-
-	std::string name;
-	name.resize(len);
-	memcpy(&name[0], nd->value() + sizeof(uint32_t), len);
-	return QueueSettings(name);
+QueueSettings QueueSettings::fromNetworkMessage(const NetworkMessage_ptr &nd) {
+  std::string name;
+  QueueSettingsScheme::read(nd->value(), name);
+  return QueueSettings(name);
 }
 
 void Queue::updateTime() {
